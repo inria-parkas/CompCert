50dddda83c3f3e4bed05401d017dd70360bba71e
Basile Pesin <basile.pesin@vertmo.org>
2025-03-19 11:01:22 +0100

    Fixed printing of floating-point constants in Clight

--- cfrontend/PrintClight.ml
+++ cfrontend/PrintClight.ml
@@ -85,9 +85,9 @@ let rec expr p (prec, e) =
   | Econst_int(n, _) ->
       fprintf p "%ld" (camlint_of_coqint n)
   | Econst_float(f, _) ->
-      fprintf p "%.18g" (camlfloat_of_coqfloat f)
+      fprintf p "%.18f" (camlfloat_of_coqfloat f)
   | Econst_single(f, _) ->
-      fprintf p "%.18gf" (camlfloat_of_coqfloat32 f)
+      fprintf p "%.18ff" (camlfloat_of_coqfloat32 f)
   | Econst_long(n, Tlong(Unsigned, _)) ->
       fprintf p "%LuLLU" (camlint64_of_coqint n)
   | Econst_long(n, _) ->
