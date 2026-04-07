7cdab2907000ca3575d1d1eedb7bbee172ca3a53
Balthazar Patiachvili <balthazar.patiachvili@crans.org>
2026-04-07 17:20:17 +0200

    Fix stack layout proof for aarch64 and riscV
    Co-authored-by: Chaarlesss <Charles.De.Haro@ens.fr>

--- riscV/Stacklayout.v
+++ riscV/Stacklayout.v
@@ -59,7 +59,7 @@ Lemma frame_env_separated:
        ** range sp (fe_ofs_callee_save fe) (size_callee_save_area b (fe_ofs_callee_save fe))
        ** P.
 Proof.
-Local Opaque Z.add Z.mul sepconj range.
+Local Opaque Z.add Z.mul sepconj range'.
   intros; simpl.
   set (w := if Archi.ptr64 then 8 else 4).
   set (olink := align (4 * b.(bound_outgoing)) w).
