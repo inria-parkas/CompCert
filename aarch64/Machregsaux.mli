e8f2f5135c07f97c09d7d6c0461190fb273f8d0c
Balthazar Patiachvili <balthazar.patiachvili@crans.org>
2026-04-08 15:55:16 +0200

    Add Machregsaux interface for aarch64

---
+++ aarch64/Machregsaux.mli
@@ -0,0 +1,15 @@
+(* *********************************************************************)
+(*                                                                     *)
+(*              The Compcert verified compiler                         *)
+(*                                                                     *)
+(*         Xavier Leroy, Collège de France and INRIA Paris             *)
+(*                                                                     *)
+(*  Copyright Institut National de Recherche en Informatique et en     *)
+(*  Automatique.  All rights reserved.  This file is distributed       *)
+(*  under the terms of the INRIA Non-Commercial License Agreement.     *)
+(*                                                                     *)
+(* *********************************************************************)
+
+(** Auxiliary functions on machine registers *)
+
+val is_scratch_register : string -> bool
